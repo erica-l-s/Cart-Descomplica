96d1d6e5cb3df00f26dd0d46717651d65da45cc7
erica-l-s <erica.l.s1996@gmail.com>
2023-07-11 21:36:50 -0300

    contador funcionando

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { BsFillCartXFill } from 'react-icons/bs'
 import { CiCirclePlus, CiCircleMinus } from 'react-icons/ci'
 import './CartItem.css'
@@ -8,6 +8,10 @@ import AppContext from '../../context/AppContext'
 function CartItem({ data }) {
     const { cartItems, setCartItems } = useContext(AppContext)
     const { id, thumbnail, title, price } = data
+    const [counter, setCount] = useState(0);
+ 
+    const add = () => setCount(counter + 1);
+    const sub = () => setCount(counter - 1);
 
     const handleRemoveItem = () => {
         const updatedItems = cartItems.filter((item) => item.id !== id)
@@ -23,9 +27,9 @@ function CartItem({ data }) {
             <div className='cart-item-content'>
                 <h3 className='cart-item-title'>{title}</h3>
                 <div className='cart-item-quanty'>
-                    <button className='button__quanty-item'><CiCircleMinus /></button>
-                    <span className='cart-item-number'>1</span>
-                    <button className='button__quanty-item'><CiCirclePlus /></button>
+                    <button className='button__quanty-item' onClick={sub}><CiCircleMinus /></button>
+                    <span className='cart-item-number'>{counter + 1}</span>
+                    <button className='button__quanty-item' onClick={add}><CiCirclePlus /></button>
                 </div>
 
 
